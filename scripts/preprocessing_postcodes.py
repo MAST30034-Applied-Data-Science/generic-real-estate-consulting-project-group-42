5ab2cf7f4aae87839ec4fb32e1aa26a2007946ae
kelmanchen <kelmanchen@outlook.com>
2022-09-07 20:03:43 +1000

    quick wd comment

--- scripts/preprocessing_postcodes.py
+++ scripts/preprocessing_postcodes.py
@@ -4,6 +4,7 @@ import pandas as pd
 # read in postcode csv 
 relative_dir = "../data/raw/external/"
 
+# to run, working directory needs to be set to /scripts
 postcode = pd.read_csv(relative_dir + 'postcode.csv', header=None)
 unique_postcode = postcode.iloc[:,0].drop_duplicates()
 unique_postcode.to_csv("../data/curated/unique_postcodes.csv", index=False, header=False)
