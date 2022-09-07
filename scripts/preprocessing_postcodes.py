49db7ee220b73baa884398a8bfaa6e521f2c1ab6
kelmanchen <kelmanchen@outlook.com>
2022-09-07 20:02:53 +1000

    fixed postcode preprocess

--- scripts/preprocessing_postcodes.py
+++ scripts/preprocessing_postcodes.py
@@ -1,13 +1,9 @@
 
-from pyspark.sql import SparkSession
 import pandas as pd
-import geopandas as gpd
-import os
 
 # read in postcode csv 
-dir_name = os.path.dirname(__file__)
 relative_dir = "../data/raw/external/"
 
-postcode = pd.read_csv(dir_name + relative_dir + 'postcode.csv', header = None)
-postcode = postcode.iloc[:,0].unique()
-postcode.to_csv("../data/curated/unique_postcodes.csv")
+postcode = pd.read_csv(relative_dir + 'postcode.csv', header=None)
+unique_postcode = postcode.iloc[:,0].drop_duplicates()
+unique_postcode.to_csv("../data/curated/unique_postcodes.csv", index=False, header=False)
