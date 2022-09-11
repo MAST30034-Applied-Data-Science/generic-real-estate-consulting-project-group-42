a6c11c336773b9cbcb64ae0a3bb9f59624a6f2f8
jwillmur <88318566+jwillmur@users.noreply.github.com>
2022-09-11 18:12:10 +1000

    ptv distances not number

--- scripts/download_ptv.py
+++ scripts/download_ptv.py
@@ -6,6 +6,7 @@ import geopandas
 import json
 import os
 
+from dotenv import load_dotenv
 from openrouteservice import client, places
 
 ## find directory
@@ -13,35 +14,57 @@ dir_name = os.path.dirname(__file__)
 output_dir = '../data/curated/'
 
 ## set variables
-api_key = ''
+load_dotenv()
+tokens = []
+for i in range(1,17): 
+    token = os.environ[f'token_{i}']
+    tokens.append(token)
 category_ids = [587, 588, 604, 607]
 buffer_size = 2000   # between 1-2000
-x = 1
-y = 2
+
+
+if not os.isfile(f'{dir_name}{output_dir}num_ptv.json'):
+    x = 1
+    y = 501
+else:
+    data = json.load(open( f"{dir_name}{output_dir}num_ptv.json" ))
+    x = len(data) + 1
+    y = len(data) + 501
+
 
 ## read apartment data
 property_data = json.load(open( f"{dir_name}{output_dir}property_metadata.json" ))
 
-for index in range(x,y):
-    coords = property_data[list(property_data.keys())[index]]['Coordinates']
+for token in tokens:
+    for index in range(x,y):
+        if index > len(property_data.keys()):
+            break
+        key = list(property_data.keys())[index]
+        coords = property_data[key]['Coordinates']
 
-    ## query code
-    ors = client.Client(key=api_key)
-    query = {'request': 'pois',
-            'geojson': {'type':'Point','coordinates':coords},
-            'buffer': buffer_size,
-            'filter_category_ids': category_ids,
-            'sortby':'distance'}
-    features = ors.places(**query)['features']
+        ## query code
+        ors = client.Client(key=token)
+        query = {'request': 'pois',
+                'geojson': {'type':'Point','coordinates':coords},
+                'buffer': buffer_size,
+                'filter_category_ids': category_ids,
+                'sortby':'distance'}
+        features = ors.places(**query)['features']
 
-    ## distances - direct meters
-    distances = []
-    for poi in features:
-        if poi['properties']['distance']<1000:
+        ## distances - direct meters
+        distances = []
+        for poi in features:
             distances.append(poi['properties']['distance'])
+        
+        ## add information
+        if len(distances)>3:
+            property_data[key]['PTV'] = distances[0:3]
+        else:
+            property_data[key]['PTV'] = distances
 
-    ## add information
-    property_data[property]['PTV'] = len(distances)
-
-## write new file
-json.dump(property_data, open(f"{dir_name}{output_dir}ptv_{x}_{y}.json", 'w'))
+    ## write new file
+    if not os.path.exists(f'{dir_name}{output_dir}num_ptv.json'):
+        json.dump(property_data, open(f"{dir_name}{output_dir}num_ptv.json", 'w'))
+    else: 
+        data.append(property_data)
+        json.dump(data, open(f"{dir_name}{output_dir}num_ptv.json", 'w'))
