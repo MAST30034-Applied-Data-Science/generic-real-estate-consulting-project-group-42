f2426a7a7919ae44639d40ef907b7578bda2cb68
kelmanchen <kelmanchen@outlook.com>
2022-09-24 15:03:38 +1000

    created sa2 postcode mapping data for 2021

--- scripts/download_external.py
+++ scripts/download_external.py
@@ -74,7 +74,7 @@ urlretrieve(url, output_dir)
 
 print('Completed downloading postcodes data')
 
-## POSTCODE TO SA2 MAPPING
+## POSTCODE TO SA2 MAPPING 2011
 url = "https://www.abs.gov.au/AUSSTATS/subscriber.nsf/log?openagent&1270055006_CG_POSTCODE_2011_SA2_2011.zip&1270.0.55.006&Data%20Cubes&70A3CE8A2E6F9A6BCA257A29001979B2&0&July%202011&27.06.2012&Latest"
 
 filename = "mapping_postcode_SA2.xlsx"
@@ -84,7 +84,43 @@ req = requests.get(url)
 with zipfile.ZipFile(BytesIO(req.content)) as sa2_zipfile:
     sa2_zipfile.extractall(output_dir)
 
-print('Completed downloading and extracting postcodes to SA2 mapping data')
+print('Completed downloading and extracting postcodes to SA2 mapping data 2011')
+
+## SA2 HISTORICAL CORRESPONDENCES
+url = "https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/correspondences/CG_SA2_2016_SA2_2021.csv"
+
+filename = "sa2_2016_to_2021.csv"
+output_dir = f"{dir_name}{relative_dir}{filename}"
+urlretrieve(url, output_dir)
+
+
+url = "https://www.abs.gov.au/AUSSTATS/subscriber.nsf/log?openagent&cg_sa2_2011_sa2_2016.zip&1270.0.55.001&Data%20Cubes&C9CFBB94B52B200DCA257FED0014C198&0&July%202016&12.07.2016&Latest"
+
+filename = "sa2_2011_to_2016"
+output_dir = f"{dir_name}{relative_dir}{filename}"
+urlretrieve(url, f'{output_dir}.zip')
+with zipfile.ZipFile(f'{output_dir}.zip', "r") as zip_ref:
+    zip_ref.extractall(f'{output_dir}')
+
+print('Completed downloading and extracting SA2 historical correspondences data')
+
+## POSTCODE HISTORICAL CORRESPONDENCES
+url = "https://www.abs.gov.au/ausstats/subscriber.nsf/log?openagent&cg_poa_2011_poa_2016.zip&1270.0.55.003&Data%20Cubes&3399EB98D8341DFFCA25802C0014C3D3&0&July%202016&13.09.2016&Previous"
+
+filename = "postcode_2011_to_2016"
+output_dir = f"{dir_name}{relative_dir}{filename}"
+urlretrieve(url, f'{output_dir}.zip')
+with zipfile.ZipFile(f'{output_dir}.zip', "r") as zip_ref:
+    zip_ref.extractall(f'{output_dir}')
+
+
+url = "https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/correspondences/CG_POA_2016_POA_2021.csv"
+
+filename = "postcode_2016_to_2021.csv"
+output_dir = f"{dir_name}{relative_dir}{filename}"
+urlretrieve(url, output_dir)
+
+print('Completed downloading and extracting postcode historical correspondences data')
 
 ## POPULATIONS
 pop_url = 'https://www.abs.gov.au/census/find-census-data/datapacks/download/2021_TSP_SA2_for_VIC_short-header.zip'
