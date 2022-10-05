a18c29877ddb469294ffd106d7fe2801c7717960
kelmanchen <kelmanchen@outlook.com>
2022-10-05 14:12:47 +1100

    moved mapping function to helper functions

--- scripts/helper_functions.py
+++ scripts/helper_functions.py
@@ -93,3 +93,29 @@ def find_poi(property_data, index, token, buffer_size, category, amenities_dict)
     time.sleep(0.5)
 
     return amenities_dict
+
+def preprocess_mapping(input_dir, column1, column2, column1_renamed, column2_renamed):
+    ''' 
+    Takes input directory, and column names as input, reads in the input directory according to the fyle type before
+    pre-processsing and returning preprocessed dataframe 
+    '''
+    if input_dir[-3:] == 'csv':
+        df = pd.read_csv(input_dir)
+    elif input_dir[-3:] == 'xls':
+        df = pd.read_excel(input_dir, sheet_name='Table 3', header=[5], engine="xlrd")
+
+    # remove NaN values 
+    df = df[~df[column1].isna()]
+    df = df[~df[column2].isna()]
+
+    # select necessary rows and columns
+    df = df[:-1]
+    df = df[[column1, column2]]
+
+    # convert to string type
+    df[column1] = df[column1].astype(int).astype(str)
+    df[column2] = df[column2].astype(int).astype(str)
+
+    df = df.rename(columns={column1: column1_renamed, column2: column2_renamed})
+
+    return df
--- scripts/preprocessing_sa2_postcode_mapping.py
+++ scripts/preprocessing_sa2_postcode_mapping.py
@@ -1,35 +1,13 @@
 import os
 import pandas as pd
+from helper_functions import preprocess_mapping
+
 
 dir_name = os.path.dirname(__file__)
 dir_name = os.path.dirname(dir_name)
 relative_dir_raw = '/data/raw/external/'
 relative_dir_curated = '/data/curated/'
 
-''' Takes input directory, and column names as input, reads in the input directory according to the fyle type before
-    pre-processsing and returning preprocessed dataframe 
-'''
-def preprocess_mapping(input_dir, column1, column2, column1_renamed, column2_renamed):
-    if input_dir[-3:] == 'csv':
-        df = pd.read_csv(input_dir)
-    elif input_dir[-3:] == 'xls':
-        df = pd.read_excel(input_dir, sheet_name='Table 3', header=[5], engine="xlrd")
-
-    # remove NaN values 
-    df = df[~df[column1].isna()]
-    df = df[~df[column2].isna()]
-
-    # select necessary rows and columns
-    df = df[:-1]
-    df = df[[column1, column2]]
-
-    # convert to string type
-    df[column1] = df[column1].astype(int).astype(str)
-    df[column2] = df[column2].astype(int).astype(str)
-
-    df = df.rename(columns={column1: column1_renamed, column2: column2_renamed})
-
-    return df
 
 # read in and preprocess all required dataframes
 sa2_postcode_mapping_2011 = preprocess_mapping(dir_name + relative_dir_raw + "1270055006_CG_POSTCODE_2011_SA2_2011.xls",
